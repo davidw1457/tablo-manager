739a057c36cdd55ee1fb7c118fc0d7711d4ea759
davidw1457 <89819150+davidw1457@users.noreply.github.com>
2024-09-04 17:24:46 -0500

    fixed typo, inline databaseDir generation since it only happens once and began work on tablo package

--- main.go
+++ main.go
@@ -6,18 +6,12 @@ import (
 )
 
 func main() {
-	databaseDir, err = databaseDirectory()
+	databaseDir, err := os.UserHomeDir()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
+	databaseDir += string(os.PathSeparator) + ".tablomanager"
 
-func databaseDirectory() (string, error) {
-	directoryRoot, err := os.UserHomeDir()
-	if err != nil {
-		return "", err
-	}
-	directoryRoot += string(os.PathSeparator) + ".tablomanager"
-	return directoryRoot, nil
+	// tablo := tablo.New(databaseDir)
 }
